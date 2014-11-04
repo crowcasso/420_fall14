0ecb598b5cf9e419205576bfc911dc91ab86f1ec
Crow Casso <crowcasso>
2014-11-04 14:55:36 -0500

    3D Example

---
+++ GameTwo_Beta2/GameTwo_Beta2/GameTwo_Beta2/ModelManager.cs
@@ -0,0 +1,72 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace GameTwo_Beta2
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class ModelManager : DrawableGameComponent
+    {
+
+        List<BasicModel> models = new List<BasicModel>();
+
+        public ModelManager(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            models.Add(new SpinningEnemy(Game.Content.Load<Model>(@"Models\spaceship")));
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            for (int i = 0; i < models.Count; ++i)
+            {
+                models[i].Update();
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (BasicModel bm in models)
+            {
+                bm.Draw(((Game1)Game).camera);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameTwo_Beta2/GameTwo_Beta2/GameTwo_Beta2/SpinningEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameTwo_Beta2
+{
+    class SpinningEnemy : BasicModel
+    {
+        Matrix rotation = Matrix.Identity;
+
+        public SpinningEnemy(Model m)
+            : base(m)
+        {
+        }
+
+        public override void Update()
+        {
+            rotation *= Matrix.CreateRotationY(MathHelper.Pi / 180);
+        }
+
+        public override Matrix GetWorld()
+        {
+            return world * rotation;
+        }
+    }
+}
